e48565f3a13de8441a66beb9952a3049ee2fef4d
JohanOrtega28 <125934568+JohanOrtega28@users.noreply.github.com>
2023-03-27 22:57:31 -0500

    Taller6Java

---
+++ src/main/java/Fabricante.java
@@ -0,0 +1,38 @@
+package vehiculos;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Fabricante {
+    String nombre;
+    Pais pais;
+    static Map<Fabricante, Integer> ventasPorFabrica = new HashMap<>();
+
+    public Fabricante(String nom, Pais p){
+        this.nombre = nom;
+        this.pais = p;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Pais getPais() {
+        return pais;
+    }
+
+    public static Fabricante fabricaMayorVentas(){
+        int valorMax = -1;
+        Fabricante fabricaMayorVentas = null;
+
+        for (Map.Entry<Fabricante, Integer> entry : ventasPorFabrica.entrySet()) {
+            int valorActual = entry.getValue();
+
+            if (valorActual > valorMax){
+                valorMax = valorActual;
+                fabricaMayorVentas = entry.getKey();
+            }
+        }
+        return fabricaMayorVentas;
+    }
+}
